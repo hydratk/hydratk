1fc1f236153a603839b044358073e86f5a5a207c
Petr Rašek <bowman@hydratk.org>
2018-07-09 20:11:26 +0200

    Modified - PyPy 3 support

--- doc/rst/install/requirements.rst
+++ doc/rst/install/requirements.rst
@@ -6,7 +6,7 @@ Requirements
 HydraTK is implemented in Python. Before you can install, you must fulfill basic requirements.
 
 * Any OS Linux - distributions Debian, Red Hat, Fedora, SuSe, Slackware, Gentoo, Arch, FreeBSD with default package manager
-* Python interpreter - versions CPython 2.6, 2.7, 3.3, 3.4, 3.5, 3.6, PyPy 2.7
+* Python interpreter - versions CPython 2.6, 2.7, 3.3, 3.4, 3.5, 3.6, PyPy 2.7, 3.5
 * Python modules - setuptools, pip, wheel (actual versions)
 
 .. note::
--- src/hydratk/core/template.py
+++ src/hydratk/core/template.py
@@ -236,7 +236,7 @@ README for {lib_ucname}
 OS and Python versions support
 ==============================
 
-| Currently the Linux platform with CPython 2.6, 2.7, 3.3, 3.4, 3.5, 3.6, PyPy 2.7 is supported, 
+| Currently the Linux platform with CPython 2.6, 2.7, 3.3, 3.4, 3.5, 3.6, PyPy 2.7, 3.5 is supported,
 | but the final version is planned to be crossplatform and targeted also to the other popular systems 
 | including Windows and MacOS and possibly other Python versions such as Jython and IronPython
 '''
@@ -651,7 +651,7 @@ README for {ext_ucname}
 OS and Python versions support
 ==============================
 
-| Currently the Linux platform with CPython 2.6, 2.7, 3.3, 3.4, 3.5, 3.6, PyPy 2.7 is supported, 
+| Currently the Linux platform with CPython 2.6, 2.7, 3.3, 3.4, 3.5, 3.6, PyPy 2.7, 3.5 is supported,
 | but the final version is planned to be crossplatform and targeted also to the other popular systems 
 | including Windows and MacOS and possibly other Python versions such as Jython and IronPython
 '''
